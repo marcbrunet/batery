511dee10b1280a1505fd5f4ae0b2d59aba0f0c13
marc <marcbrunet80@gmail.com>
2022-10-17 23:47:11 +0200

    start data in dashboard.html

--- cells/urls.py
+++ cells/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('json', views.series_capacyti_json, name='capacity'),
     path('', views.series_capacyti, name='capacity'),
 ]
--- cells/views.py
+++ cells/views.py
@@ -1,11 +1,9 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from cells.models import series, pack, evaluation
 from django.http import JsonResponse
-# Create your views here.
-
-
-def series_capacyti(request):
-    data = {'series': {}}
+# functions
+def getSeriesData(data):
     paralels = series.objects.all()
     for s in paralels:
         packs = pack.objects.filter(series=s)
@@ -14,4 +12,19 @@ def series_capacyti(request):
             if not elemnt.retiret:
                 capacyti = evaluation.objects.filter(pack_id=elemnt).last().capacyti
                 data['series'][s.number] += capacyti
+
+
+# views here.
+def series_capacyti(request):
+    data = {'series': {}, 'grafNames': [], 'grafValues': []}
+    getSeriesData(data)
+    for k, v in data['series'].items():
+        data['grafNames'].append(("S"+str(k)))
+        data['grafValues'].append(v)
     return render(request=request, template_name='dashboard.html', context=data)
+
+def series_capacyti_json(request):
+    data = {'series': {}}
+    getSeriesData(data)
+    return JsonResponse(data)
+
